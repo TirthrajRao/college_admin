f617c755bc2726c38420f89a26eb5619eab1bac3
yashshukla <yashs018@gmail.com>
2017-03-22 11:20:28 +0530

    config change

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -1,64 +1,70 @@
 angular.module('collegeAdmin')
-  .config(['$stateProvider','$urlRouterProvider','$locationProvider',function($stateProvider,$urlRouterProvider,$locationProvider){
-
+.config(['$stateProvider','$urlRouterProvider','$locationProvider','$httpProvider',function($stateProvider,$urlRouterProvider,$locationProvider,$httpProvider){
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+  $httpProvider.interceptors.push('noCacheInterceptor');
 
   $stateProvider
   .state('account',{
-        url : '/account',
-        views:{
-          'top' : {
-            templateUrl : 'account/view/menu.html',
-            controller  : 'logoutCtrl'
-          },
+    url : '/account',
+    views:{
+      'top' : {
+        templateUrl : 'account/view/menu.html',
+        controller  : 'logoutCtrl'
+      },
 
-          'bottom' : {
-            template :'<ui-view></ui-view>'
-    
-          }
-        }
-      })
-      .state('admin',{
-        url : '/admin',
-        views:{
-          'top' : {
-            templateUrl : 'account/view/menu.html',
-          controller  : 'logoutCtrl'
-          },
+      'bottom' : {
+        template :'<ui-view></ui-view>'
 
-          
+      }
+    }
+  })
+  .state('admin',{
+    url : '/admin',
+    views:{
+      'top' : {
+        templateUrl : 'account/view/menu.html',
+        controller  : 'logoutCtrl'
+      },
 
-          'bottom' : {
-            template :'<ui-view></ui-view>',
-          }
-        }
-      });
+
+
+      'bottom' : {
+        template :'<ui-view></ui-view>',
+      }
+    }
+  });
 
 //    $locationProvider.html5Mode(true);
-      $urlRouterProvider.otherwise('/admin/login');
+$urlRouterProvider.otherwise('/admin/login');
 
-  }])
-  .run(['$rootScope','$localStorage','$state','$stateParams',function($rootScope,$localStorage,$state,$stateParams){
-  		$rootScope.serverUrl = 'http://localhost/college/collegeAdminNew/server/admin/';
- 
-      $rootScope.$state = $state;
-      $rootScope.$stateParams = $stateParams;
-      $rootScope.$on('$stateChangeSuccess',function(event , toState ,fromState){  
-        if(toState.name.localeCompare("admin.login") == 0 ){
-          if(toState.name.substr(0,7).localeCompare('account')==0 || toState.name.substr(0,5).localeCompare('admin')==0){
-          console.log(toState);
-          console.log(fromState);
-          }       
-          else{
-          if(typeof($localStorage.adminId) == undefined || $localStorage.adminId == null){
-            console.log(typeof($localStorage.adminId));
-            $state.go('admin.login');
-            }
-          }
+}])
+.run(['$rootScope','$localStorage','$state','$stateParams',function($rootScope,$localStorage,$state,$stateParams){
+  $rootScope.serverUrl = 'http://132.140.160.109/college/collegeAdminNew/server/admin/';
+
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+  $rootScope.$on('$stateChangeSuccess',function(event , toState ,fromState){  
+    if(toState.name.localeCompare("admin.login") == 0 )
+    {
+    console.log(toState.name);
+      console.log("if 1");
+      if(toState.name.substr(0,7).localeCompare('account') == 0 || toState.name.substr(0,5).localeCompare('admin')==0)
+      {
+        console.log(toState);
+        console.log(fromState);
+      }       
+      else{
+        console.log($localStorage.adminId);
+        if(typeof($localStorage.adminId) == undefined || $localStorage.adminId == null){
+          console.log(typeof($localStorage.adminId));
+          $state.go('admin.login');
         }
-        });
+      }
+    }
+  });
 
 
 
 
 
-   	}]);
+}]);
