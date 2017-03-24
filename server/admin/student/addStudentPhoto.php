3961594dd19bb2d1a7218e74e4670951c53d87ff
yashshukla <yashs018@gmail.com>
2017-03-24 23:09:10 +0530

    webcame retrive

--- server/admin/student/addStudentPhoto.php
+++ server/admin/student/addStudentPhoto.php
@@ -2,12 +2,12 @@
 header("Access-Control-Allow-Origin: *");
 header("Cache-Control: *"); //HTTP 1.1
 
-  include("config.php");
-  include("functions.php");
-  $data = file_get_contents("php://input");
-  $data = json_decode($data,true);
-  $sid = $_GET['sid'];
-   $courseid = getCourseFromSid($con,$sid);
+include("config.php");
+include("functions.php");
+$data = file_get_contents("php://input");
+$data = json_decode($data,true);
+$sid = $_GET['sid'];
+$courseid = getCourseFromSid($con,$sid);
 $imgData1 = $data['imageData'];
 
 //$output_file = '../../studentPhoto/'.$courseid.'/'.$academicYear;
@@ -18,41 +18,42 @@ $imgData1 = $data['imageData'];
 
 
 
-  $sid = $_GET['sid'];
+$sid = $_GET['sid'];
 
-  $courseid = getCourseFromSid($con,$sid);
-  $name = getNameFromSid($con,$sid);
-  $dob = getDobFromSid($con,$sid);
- 
-  $fullname = explode(" ", $name);
-  $lname = $fullname[0];
-  $fname = $fullname[1];
+$courseid = getCourseFromSid($con,$sid);
+$name = getNameFromSid($con,$sid);
+$dob = getDobFromSid($con,$sid);
 
-  if (!file_exists('../../studentPhoto/'.$courseid.'/'.$academicYear)) {
-    exec(mkdir('../../studentPhoto/'.$courseid.'/'.$academicYear,766, true));
+$fullname = explode(" ", $name);
+$lname = $fullname[0];
+$fname = $fullname[1];
+
+if (!file_exists('../../studentPhoto/'.$courseid.'/'.$academicYear)) {
+  exec(mkdir('../../studentPhoto/'.$courseid.'/'.$academicYear,0777, true));
 }
-  $upath='../../studentPhoto/'.$courseid.'/'.$academicYear.'/';
-  $photoPath = 'studentPhoto/'.$courseid.'/'.$academicYear.'/';
-  $name = $_FILES['file']['name'];
-  $newName = $lname.'_'.$fname.'_'.$sid.'.jpg';
-  $target = $upath.$name;
-  if($imgData1 != ''){
-    $imgData1 = substr($imgData1, strrpos($imgData1, ','));
-      file_put_contents($upath.$newName, base64_decode($imgData1));
-      echo "webcame image";
-      mysqli_query($con,"UPDATE `student` set `path`='$photoPath$newName' where sid = '$sid'") or die(mysqli_error($con));
+$upath='../../studentPhoto/'.$courseid.'/'.$academicYear.'/';
+$photoPath = 'studentPhoto/'.$courseid.'/'.$academicYear.'/';
+$name = $_FILES['file']['name'];
+$newName = $lname.'_'.$fname.'_'.$sid.'.jpg';
+$target = $upath.$name;
+if($imgData1 != ''){
+  $imgData1 = substr($imgData1, strrpos($imgData1, ','));
+  file_put_contents($upath.$newName, base64_decode($imgData1));
+  echo "webcame image";
+  chmod($upath.$newName, 0777);
+  mysqli_query($con,"UPDATE `student` set `path`='$photoPath$newName' where sid = '$sid'") or die(mysqli_error($con));
 
 
-  }
-  if(move_uploaded_file($_FILES['file']['tmp_name'],$target)){
-    $result = exec(rename($upath.$name,$upath.$newName));
-    chmod($target, 0777);
-   
-   mysqli_query($con,"UPDATE `student` set `path` = '$photoPath$newName' where sid = '$sid'") or die(mysqli_error($con));
-    echo "Success";
-  }
-  else{
-   echo "Failed to upload.";
-  }
+}
+if(move_uploaded_file($_FILES['file']['tmp_name'],$target)){
+  $result = exec(rename($upath.$name,$upath.$newName));
+  chmod($upath.$newName, 0777);
+
+  mysqli_query($con,"UPDATE `student` set `path` = '$photoPath$newName' where sid = '$sid'") or die(mysqli_error($con));
+  echo "Success";
+}
+else{
+ echo "Failed to upload.";
+}
 
 ?>
